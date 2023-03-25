590f8b2158c1f055e82f5ec59b11db9aa78da49e
aholake <nluit.tanloc@gmail.com>
2023-03-25 23:14:36 +0700

    Add deployment.yaml

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -6,8 +6,16 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/users/:id', (req, res) => {
   res.send(createRandomUser());
+});
+
+app.get('/users', (req, res) => {
+  const users = [];
+  for (let i = 0; i < 10; i++) {
+    users.push(createRandomUser());
+  }
+  res.send(users);
 });
 
 app.listen(process.env.PORT, () => {
